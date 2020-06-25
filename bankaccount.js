47ff4242c11bb3468ef10d2f5a7a3974c659467c
unknown <michael.messina@hotmail.fr>
2020-06-25 16:57:46 +0200

    refacto + creation objet generique BankAccunt

--- bankaccount.js
+++ bankaccount.js
@@ -1,14 +1,17 @@
 const deposit = require('./operations/deposit')
 const withdraw = require('./operations/withdraw')
-const bankaccount = {
-    balance: 50,
-    balanceFormat(){
-        return (this.balance).toFixed(2) + " €"
-    },
-    history:[],
+class BankAccount {
+    constructor(balance) {
+        this.balance = balance;
+        this.history = [];
+    }
 }
-deposit(bankaccount, 155.1234);
-deposit(bankaccount, 100);
-withdraw(bankaccount, 55.50)
-exports.bankaccount = bankaccount;
-console.log(bankaccount.history);
+const MyBankAccount = new BankAccount(50)
+
+// session de test
+deposit(MyBankAccount, 155.1234);
+deposit(MyBankAccount, 100);
+withdraw(MyBankAccount, 55.50)
+console.log(MyBankAccount.history);
+
+exports.bankaccount = BankAccount;
