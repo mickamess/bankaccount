9150e44450485d6f4ffdfb1e49dfa4531ee80ed1
mickamess <63344915+mickamess@users.noreply.github.com>
2020-06-25 16:35:06 +0200

    Update withdraw.js

--- operations/withdraw.js
+++ operations/withdraw.js
@@ -2,5 +2,9 @@ module.exports = function withdraw(account, withdrawAmount) {
     if ( withdrawAmount <= 0) {
         return '! please enter a positive amount !'
     }
+        if ( withdrawAmount>account.balance)
+    {
+        return `your current balance (${account.balance}) insuffisant for this withdrawal`
+    }
 account.balance -= withdrawAmount;
 }
